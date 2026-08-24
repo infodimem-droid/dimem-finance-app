247a3e6daa22646c0fbb3a8d504c5376b1c7ce1e
integraltcc-a11y <integraltcc@gmail.com>
2026-08-24 15:05:09 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FinanceLedger from './FinanceLedger.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FinanceLedger />
+  </React.StrictMode>
+)
